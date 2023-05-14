3b75124db242c800129edd99f9b3aea308bc8936
Trevor  Keegan <positev@outlook.com>
2023-05-14 17:39:40 -0400

    format with black

--- src/authService.py
+++ src/authService.py
@@ -3,123 +3,116 @@ from datetime import date, timedelta
 from dataclasses import dataclass
 from typing import List
 
-    
+
 class Cryptographer:
-    
     def __init__(self, secret_key):
         self.secret_key = secret_key
-    
+
     def encrypt(self, plain_text: str) -> str:
-        #TODO: Actually encrypt the string
+        # TODO: Actually encrypt the string
         return plain_text.swapcase()
-    
+
+
 @dataclass
 class User:
     first_name: str
     last_name: str
     email: str
     encrypted_pass: str
-    
-    
+
     def to_json(self):
         return {
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'email': self.email
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
         }
+
 
 @dataclass
 class Token:
     token: str
     expires_on: date
-    
+
     def to_json(self):
-        return {
-            'token': self.token,
-            'expires_on': self.expires_on
-        }
-        
-        
+        return {"token": self.token, "expires_on": self.expires_on}
+
+
 class AuthenticationTokenHandler:
     def generate_token(self, user: User) -> Token:
-        #TODO: Actually make a token
+        # TODO: Actually make a token
         return Token(f"{user.email}_123456789", date.today() + timedelta(days=180))
-    
-        
+
+
 class UserPerstance:
-    
-    #TODO: Actually store things in a database
-    #TODO: Encrypt data before it is put into the database and decrypt it after
-    
+    # TODO: Actually store things in a database
+    # TODO: Encrypt data before it is put into the database and decrypt it after
+
     def __init__(self):
         self.user_map = {}
-    
+
     def user_is_known(self, email):
         return email in self.user_map
-        
+
     def get_user(self, email) -> User:
         if self.user_is_known(email):
             user_struct = self.user_map[email]
-            user = user_struct['user']
+            user = user_struct["user"]
             return user
-        
+
         return None
 
     def get_tokens(self, email) -> List[Token]:
         if self.user_is_known(email):
             user_struct = self.user_map[email]
-            tokens = user_struct['tokens']
+            tokens = user_struct["tokens"]
             return tokens
-        
+
         return []
-        
+
     def add_token_to_user(self, email, token) -> bool:
         user = self.get_user(email)
         if user is not None:
-            self.user_map[email]['tokens'].append(token)
+            self.user_map[email]["tokens"].append(token)
             return True
-    
+
     def add_user(self, user: User) -> bool:
         if not self.user_is_known(user.email):
-            self.user_map[user.email] = {
-                'user': user,
-                'tokens': []
-            }
-            
+            self.user_map[user.email] = {"user": user, "tokens": []}
+
             return True
         return False
-    
+
 
 class UserService:
     def __init__(self, crypto, token_factory, persistance):
         self.crypto = crypto
         self.token_factory = token_factory
         self.persistance = persistance
-        
+
     def create_user(self, first_name, last_name, email, password) -> User:
         encrypted_pass = crypto.encrypt(password)
         user = User(first_name, last_name, email, encrypted_pass)
-        if (self.persistance.add_user(user)):
+        if self.persistance.add_user(user):
             return user
         else:
             return None
-        
+
     def get_user(self, email):
         return self.persistance.get_user(email)
-        
+
     def user_does_exist(self, email) -> bool:
         return self.persistance.get_user(email) is not None
-        
+
     def authenticate_user(self, email, password=None, token=None) -> Token:
-        user  = self.persistance.get_user(email)
+        user = self.persistance.get_user(email)
         if user is not None:
             encrypted_pass = crypto.encrypt(password)
             if user.encrypted_pass == encrypted_pass:
                 new_token = self.get_new_token(user)
                 return new_token
-        
+
         return None
-    
+
     def get_new_token(self, user) -> Token:
         token = self.token_factory.generate_token(user)
         self.persistance.add_token_to_user(user.email, token)
@@ -129,46 +122,42 @@ class UserService:
         tokens = self.persistance.get_tokens(email)
         return any([token == ctoken.token for ctoken in tokens])
 
+
 app = Flask(__name__)
 
 persister = UserPerstance()
-crypto = Cryptographer('SECRET_TUNNEL')
+crypto = Cryptographer("SECRET_TUNNEL")
 authenticator = AuthenticationTokenHandler()
 user_service = UserService(crypto, authenticator, persister)
 
 
-@app.route('/users/new', methods=['POST'])
+@app.route("/users/new", methods=["POST"])
 def create_user():
-    
     request_data = request.json
-    
+
     user = user_service.create_user(**request_data)
     if user is not None:
         token = user_service.get_new_token(user)
         response_payload = token.to_json()
         return response_payload, 200
-    
+
     else:
-        return {'message': "An account with that email already exists."}, 409
+        return {"message": "An account with that email already exists."}, 409
 
 
-@app.route('/users/verify', methods=['POST'])
+@app.route("/users/verify", methods=["POST"])
 def verify_user():
-    
     request_data = request.json
     verified = user_service.verify_token(**request_data)
-    response_payload = {
-        'verified': verified
-    }
-    
+    response_payload = {"verified": verified}
+
     return response_payload, 200
 
 
-@app.route('/users/login', methods=['POST'])
+@app.route("/users/login", methods=["POST"])
 def authenticate_user():
-    
     request_data = request.json
-    if not user_service.user_does_exist(request_data['email']):
+    if not user_service.user_does_exist(request_data["email"]):
         message = {"message": "There is no account with that email."}
         return message, 409
     else:
@@ -179,13 +168,12 @@ def authenticate_user():
         else:
             message = {"message": "Incorrect credentials."}
             return message, 409
-        
 
-@app.route('/users/user_info', methods=['POST'])
+
+@app.route("/users/user_info", methods=["POST"])
 def lookup_user():
-    
     request_data = request.json
-    email = request_data['email']
+    email = request_data["email"]
     if not user_service.user_does_exist(email):
         message = {"message": "There is no account with that email."}
         return message, 409
@@ -198,5 +186,3 @@ def lookup_user():
         else:
             message = {"message": "Incorrect credentials."}
             return message, 409
-            
-
--- src/test_AuthService.py
+++ src/test_AuthService.py
@@ -3,232 +3,266 @@ import pytest
 from src.authService import app, persister
 from datetime import date, timedelta
 
-#must clear the user map so that adding the same test user multiple times doesnt break the test suite
+
+# must clear the user map so that adding the same test user multiple times doesnt break the test suite
 @pytest.fixture(autouse=True)
 def reset_test_env():
     persister.user_map.clear()
 
+
 # test that i can add a user and verify with the auth token i am given
 def test_adding_a_user():
     client = app.test_client()
-        
-    response = client.post('/users/new', json={
-        'first_name': 'duckey',
-        'last_name': 'beaver',
-        'email': 'duckey@olivesToys.com',
-        'password': 'mollyAndOliveAreBuddies'
-    })
-    
-    required_keys = ['expires_on', 'token']
+
+    response = client.post(
+        "/users/new",
+        json={
+            "first_name": "duckey",
+            "last_name": "beaver",
+            "email": "duckey@olivesToys.com",
+            "password": "mollyAndOliveAreBuddies",
+        },
+    )
+
+    required_keys = ["expires_on", "token"]
     assert all([x in response.json for x in required_keys])
-    token = response.json['token']
+    token = response.json["token"]
     assert response.status_code == 200
-    
-    response = client.post('/users/verify', json={
-        'token': token,
-        'email': 'duckey@olivesToys.com'
-    })
-    
+
+    response = client.post(
+        "/users/verify", json={"token": token, "email": "duckey@olivesToys.com"}
+    )
+
     assert response.status_code == 200
-    assert response.json['verified'] == True
-    
-    
+    assert response.json["verified"] == True
 
 
 # test that a wrong token will not verify
 def test_a_incorrect_token_will_not_verify():
     client = app.test_client()
-        
-    response = client.post('/users/new', json={
-        'first_name': 'duckey',
-        'last_name': 'beaver',
-        'email': 'chickum@olivesToys.com',
-        'password': 'mollyAndOliveAreBuddies'
-    })
-    
-    required_keys = ['expires_on', 'token']
+
+    response = client.post(
+        "/users/new",
+        json={
+            "first_name": "duckey",
+            "last_name": "beaver",
+            "email": "chickum@olivesToys.com",
+            "password": "mollyAndOliveAreBuddies",
+        },
+    )
+
+    required_keys = ["expires_on", "token"]
     assert all([x in response.json for x in required_keys])
-    token = response.json['token']
+    token = response.json["token"]
     assert response.status_code == 200
-    
-    response = client.post('/users/verify', json={
-        'token': "INVALID_TOKEN",
-        'email': 'duckey@olivesToys.com'
-    })
-    
+
+    response = client.post(
+        "/users/verify",
+        json={"token": "INVALID_TOKEN", "email": "duckey@olivesToys.com"},
+    )
+
     assert response.status_code == 200
-    assert response.json['verified'] == False
-    
+    assert response.json["verified"] == False
+
 
 # test that a wrong token will not verify
 def test_a_different_users_token_will_not_verify():
     client = app.test_client()
-        
-    response = client.post('/users/new', json={
-        'first_name': 'duckey',
-        'last_name': 'beaver',
-        'email': 'chickum@olivesToys.com',
-        'password': 'mollyAndOliveAreBuddies'
-    })
-    
-    token = response.json['token']
-    
-    response = client.post('/users/new', json={
-        'first_name': 'duckey',
-        'last_name': 'beaver2',
-        'email': 'beaver@olivesToys.com',
-        'password': 'mollyAndOliveAreBuddies'
-    })
-    
-    
-    response = client.post('/users/verify', json={
-        'token': token,
-        'email': 'beaver@olivesToys.com'
-    })
-    
+
+    response = client.post(
+        "/users/new",
+        json={
+            "first_name": "duckey",
+            "last_name": "beaver",
+            "email": "chickum@olivesToys.com",
+            "password": "mollyAndOliveAreBuddies",
+        },
+    )
+
+    token = response.json["token"]
+
+    response = client.post(
+        "/users/new",
+        json={
+            "first_name": "duckey",
+            "last_name": "beaver2",
+            "email": "beaver@olivesToys.com",
+            "password": "mollyAndOliveAreBuddies",
+        },
+    )
+
+    response = client.post(
+        "/users/verify", json={"token": token, "email": "beaver@olivesToys.com"}
+    )
+
     assert response.status_code == 200
-    assert response.json['verified'] == False
-    
+    assert response.json["verified"] == False
+
 
 # test that when adding a new user, if the email is already registered, then do not provide auth token
 def test_emails_must_be_unique():
     client = app.test_client()
-        
-    response = client.post('/users/new', json={
-        'first_name': 'duckey',
-        'last_name': 'beaver',
-        'email': 'chickum@olivesToys.com',
-        'password': 'mollyAndOliveAreBuddies'
-    })
-    
-    required_keys = ['expires_on', 'token']
+
+    response = client.post(
+        "/users/new",
+        json={
+            "first_name": "duckey",
+            "last_name": "beaver",
+            "email": "chickum@olivesToys.com",
+            "password": "mollyAndOliveAreBuddies",
+        },
+    )
+
+    required_keys = ["expires_on", "token"]
     assert all([x in response.json for x in required_keys])
     assert response.status_code == 200
-    
-    response = client.post('/users/new', json={
-        'first_name': 'Daddy',
-        'last_name': 'Dolphin',
-        'email': 'chickum@olivesToys.com',
-        'password': 'mollyAndOliveAreBuddies'
-    })
-    
+
+    response = client.post(
+        "/users/new",
+        json={
+            "first_name": "Daddy",
+            "last_name": "Dolphin",
+            "email": "chickum@olivesToys.com",
+            "password": "mollyAndOliveAreBuddies",
+        },
+    )
+
     assert response.status_code == 409
-    required_keys = ['expires_on', 'token']
+    required_keys = ["expires_on", "token"]
     assert not any([x in response.json for x in required_keys])
-    assert 'message' in response.json
-    assert response.json['message'] == 'An account with that email already exists.'
-    
+    assert "message" in response.json
+    assert response.json["message"] == "An account with that email already exists."
+
+
 # test getting the details for a user
+
 
 def test_a_user_can_log_in_if_they_haven_an_account():
     client = app.test_client()
-        
-    response = client.post('/users/new', json={
-        'first_name': 'duckey',
-        'last_name': 'beaver',
-        'email': 'duckey@olivesToys.com',
-        'password': 'mollyAndOliveAreBuddies'
-    })
-    
-    response = client.post('/users/login', json={
-        'email': 'duckey@olivesToys.com',
-        'password': 'mollyAndOliveAreBuddies'
-    })
-    
+
+    response = client.post(
+        "/users/new",
+        json={
+            "first_name": "duckey",
+            "last_name": "beaver",
+            "email": "duckey@olivesToys.com",
+            "password": "mollyAndOliveAreBuddies",
+        },
+    )
+
+    response = client.post(
+        "/users/login",
+        json={"email": "duckey@olivesToys.com", "password": "mollyAndOliveAreBuddies"},
+    )
+
     assert response.status_code == 200
-    required_keys = ['expires_on', 'token']
+    required_keys = ["expires_on", "token"]
     assert all([x in response.json for x in required_keys])
+
 
 def test_a_user_can_not_log_in_if_they_dont_have_an_account():
     client = app.test_client()
-    
-    response = client.post('/users/login', json={
-        'email': 'duckey@olivesToys.com',
-        'password': 'mollyAndOliveAreBuddies'
-    })
-    
+
+    response = client.post(
+        "/users/login",
+        json={"email": "duckey@olivesToys.com", "password": "mollyAndOliveAreBuddies"},
+    )
+
     assert response.status_code == 409
-    assert 'message' in response.json
-    assert response.json['message'] == 'There is no account with that email.'
+    assert "message" in response.json
+    assert response.json["message"] == "There is no account with that email."
+
 
 def test_a_user_can_not_log_in_if_they_have_the_wrong_password():
     client = app.test_client()
-    
-    response = client.post('/users/new', json={
-        'first_name': 'duckey',
-        'last_name': 'beaver',
-        'email': 'duckey@olivesToys.com',
-        'password': 'mollyAndOliveNOTAreBuddies'
-    })
-    
-    response = client.post('/users/login', json={
-        'email': 'duckey@olivesToys.com',
-        'password': 'mollyAndOliveAreBuddies'
-    })
-    
+
+    response = client.post(
+        "/users/new",
+        json={
+            "first_name": "duckey",
+            "last_name": "beaver",
+            "email": "duckey@olivesToys.com",
+            "password": "mollyAndOliveNOTAreBuddies",
+        },
+    )
+
+    response = client.post(
+        "/users/login",
+        json={"email": "duckey@olivesToys.com", "password": "mollyAndOliveAreBuddies"},
+    )
+
     assert response.status_code == 409
-    assert 'message' in response.json
-    assert response.json['message'] == 'Incorrect credentials.'
+    assert "message" in response.json
+    assert response.json["message"] == "Incorrect credentials."
+
 
 def test_get_into_of_a_user_that_does_not_exist():
     client = app.test_client()
-    
-    response = client.post('/users/user_info', json={
-        'email': 'duckey@olivesToys.com',
-        'token': '982034576_fgasdfa'
-    })
-    
+
+    response = client.post(
+        "/users/user_info",
+        json={"email": "duckey@olivesToys.com", "token": "982034576_fgasdfa"},
+    )
+
     assert response.status_code == 409
-    assert 'message' in response.json
-    assert response.json['message'] == 'There is no account with that email.'
+    assert "message" in response.json
+    assert response.json["message"] == "There is no account with that email."
+
 
 def test_get_info_of_a_user_that_exists():
     client = app.test_client()
-    
-    client.post('/users/new', json={
-        'first_name': 'duckey',
-        'last_name': 'beaver',
-        'email': 'duckey@olivesToys.com',
-        'password': 'mollyAndOliveNOTAreBuddies'
-    })
-    
-    
-    response = client.post('/users/login', json={
-        'email': 'duckey@olivesToys.com',
-        'password': 'mollyAndOliveNOTAreBuddies'
-    })
-    
-    token = response.json['token']
-    
-    response = client.post('/users/user_info', json={
-        'email': 'duckey@olivesToys.com',
-        'token': token
-    })
-    
+
+    client.post(
+        "/users/new",
+        json={
+            "first_name": "duckey",
+            "last_name": "beaver",
+            "email": "duckey@olivesToys.com",
+            "password": "mollyAndOliveNOTAreBuddies",
+        },
+    )
+
+    response = client.post(
+        "/users/login",
+        json={
+            "email": "duckey@olivesToys.com",
+            "password": "mollyAndOliveNOTAreBuddies",
+        },
+    )
+
+    token = response.json["token"]
+
+    response = client.post(
+        "/users/user_info", json={"email": "duckey@olivesToys.com", "token": token}
+    )
+
     assert response.status_code == 200
     assert response.json == {
-        'first_name': 'duckey',
-        'last_name': 'beaver',
-        'email': 'duckey@olivesToys.com'
+        "first_name": "duckey",
+        "last_name": "beaver",
+        "email": "duckey@olivesToys.com",
     }
-    
+
 
 def test_get_info_for_user_that_exists_but_authentication_fails():
     client = app.test_client()
-    
-    client.post('/users/new', json={
-        'first_name': 'duckey',
-        'last_name': 'beaver',
-        'email': 'duckey@olivesToys.com',
-        'password': 'mollyAndOliveNOTAreBuddies'
-    })
-    
-    response = client.post('/users/user_info', json={
-        'email': 'duckey@olivesToys.com',
-        'token': "INCORRECT_TOKEN"
-    })
-    
+
+    client.post(
+        "/users/new",
+        json={
+            "first_name": "duckey",
+            "last_name": "beaver",
+            "email": "duckey@olivesToys.com",
+            "password": "mollyAndOliveNOTAreBuddies",
+        },
+    )
+
+    response = client.post(
+        "/users/user_info",
+        json={"email": "duckey@olivesToys.com", "token": "INCORRECT_TOKEN"},
+    )
+
     assert response.status_code == 409
     assert response.json == {
-        'message': 'Incorrect credentials.',
+        "message": "Incorrect credentials.",
     }
